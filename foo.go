9850fa18cdb7c73c4b78c891faabb7ee32bb47e5
joshcarp <joshuacarpeggiani@gmail.com>
2021-08-02 22:55:19 +1000

    Initial commit

---
+++ foo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+type Foo struct {
+
+}
+func Blah(s int, a Foo) (int, error) {
+	return 0, fmt.Errorf("")
+}
+
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/joshcarp/muster
+
+go 1.16
+
+require golang.org/x/tools v0.1.5
